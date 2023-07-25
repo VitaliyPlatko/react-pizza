dae125512d0e523e3b3237615da1fdc3609e2da5
Vitaliy Platko <vitalikplatko@ukr.net>
2023-07-25 11:27:24 +0300

    Зробив вибір категорій, переключення між розмірами піц та видами

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './scss/app.scss'
+import Header from "./components/Header";
+import Categories from './components/Categories';
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+import pizzas from './assets/pizzas.json'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories/>
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {/* Так як я рендерю оєкт я пишу obj, це не обовязково, просто шоб було простіше */}
+            {pizzas.map((obj)=>(
+                <PizzaBlock {...obj}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
